56fd28237494e516fb036569444e8e376e210b65
Johnathan Williams <jlwill86@gmail.com>
2023-10-10 16:53:46 -0400

    rendered puppy names on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  
-
+  const [puppies, setPuppies] = useState(puppyList)
+console.log(puppies)
   return (
     
-      <div>
-        
+      <div className='App'>
+        { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>;
+   })
+}
       </div>
 
     
